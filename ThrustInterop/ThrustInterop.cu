db47f549c20e65d7ba39171ce9a0041ef478c8a1
aokomoriuta_lab2 <internal@enmps.net>
2012-11-08 11:26:16 +0900

    複数GPU対応

--- ThrustInterop/ThrustInterop.cu
+++ ThrustInterop/ThrustInterop.cu
@@ -1,3 +1,4 @@
+#include<cuda_runtime_api.h>
 #include<thrust/device_vector.h>
 #include<thrust/fill.h>
 #include<thrust/copy.h>
@@ -7,30 +8,44 @@ typedef thrust::device_vector<double> Vector;
 
 extern "C"
 {
-	__declspec(dllexport) Vector* _stdcall Create(int size)
+	__declspec(dllexport) int _stdcall GetDeviceCount()
 	{
+		int count;
+		::cudaGetDeviceCount(&count);
+
+		return count;
+	}
+
+	__declspec(dllexport) Vector* _stdcall Create(int size, int deviceID)
+	{
+		::cudaSetDevice(deviceID);
+
 		Vector* vec = new Vector(size);
 
 		return vec;
 	}
 	
-	__declspec(dllexport) void _stdcall Fill(Vector* vec, int size, double value)
+	__declspec(dllexport) void _stdcall Fill(Vector* vec, int size, double value, int deviceID)
 	{
+		::cudaSetDevice(deviceID);
 		thrust::fill_n(vec->begin(), size, value);
 	}
 	
-	__declspec(dllexport) double _stdcall Length2(Vector* vec, int size)
+	__declspec(dllexport) double _stdcall Length2(Vector* vec, int size, int deviceID)
 	{
+		::cudaSetDevice(deviceID);
 		return thrust::inner_product(vec->begin(),vec->begin() + size, vec->begin(), 0.0);
 	}
 
-	__declspec(dllexport) void _stdcall CopyTo(Vector* source, double destination[], int size)
+	__declspec(dllexport) void _stdcall CopyTo(Vector* source, double destination[], int size, int deviceID)
 	{
+		::cudaSetDevice(deviceID);
 		thrust::copy_n(source->begin(), size, destination);
 	}
 	
-	__declspec(dllexport) void _stdcall Delete(Vector* vec)
+	__declspec(dllexport) void _stdcall Delete(Vector* vec, int deviceID)
 	{
+		::cudaSetDevice(deviceID);
 		delete vec;
 	}
 }
--- ThrustInteropTest/ThrustInteropTest.cs
+++ ThrustInteropTest/ThrustInteropTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Linq;
 
 namespace LWisteria.ThrustInterop
 {
@@ -8,36 +9,87 @@ namespace LWisteria.ThrustInterop
 		const string DLL_NAME = "ThrustInterop.dll";
 
 		[DllImport(DLL_NAME)]
-		static extern IntPtr Create(int size);
+		static extern int GetDeviceCount();
 
 		[DllImport(DLL_NAME)]
-		static extern void Fill(IntPtr vec, int size, double value);
+		static extern IntPtr Create(int size, int deviceID);
 
 		[DllImport(DLL_NAME)]
-		static extern double Length2(IntPtr vec, int size);
+		static extern void Fill(IntPtr vec, int size, double value, int deviceID);
 
 		[DllImport(DLL_NAME)]
-		static extern void CopyTo(IntPtr vec, double[] array, int size);
+		static extern double Length2(IntPtr vec, int size, int deviceID);
 
 		[DllImport(DLL_NAME)]
-		static extern void Delete(IntPtr vec);
+		static extern void Delete(IntPtr vec, int deviceID);
 
-		const int N = 1300;
-		const double VALUE = 9.56;
+		const int N = 12345678 * 2;
+
+		const double VALUE = 0.0956;
+
+		const int ITRATION = 1000;
 
 		static int Main()
 		{
-			IntPtr vec = Create(N);
+			// 1GPU
+			{
+				var vec = Create(N, 0);
 
-			Fill(vec, N, VALUE);
+				Fill(vec, N, VALUE, 0);
 
-			double result = Length2(vec, N);
-			Console.WriteLine("result = {0}, answer = {1}", result, N * VALUE * VALUE);
+				double result = 0;
+				var stopwatch = new System.Diagnostics.Stopwatch();
 
-			var array = new double[N];
-			CopyTo(vec, array, N);
+				stopwatch.Start();
+				for(int i = 0; i < ITRATION; i++)
+				{
+					result += Length2(vec, N, 0);
+				}
+				stopwatch.Stop();
 
-			Delete(vec);
+				Console.WriteLine("1GPU: {0}", stopwatch.ElapsedMilliseconds);
+				Console.WriteLine("result = {0}", result);
+				Console.WriteLine("answer = {0}", VALUE * VALUE * ITRATION * N);
+
+				Delete(vec, 0);
+			}
+
+			// 複数GPU
+			{
+				int deviceCount = GetDeviceCount();
+
+				var vecs = new IntPtr[deviceCount];
+
+				for(int deviceID = 0; deviceID < deviceCount; deviceID++)
+				{
+					vecs[deviceID] = Create(N / deviceCount, deviceID);
+
+					Fill(vecs[deviceID], N / deviceCount, VALUE, deviceID);
+				}
+
+				var results = new double[deviceCount];
+
+				var stopwatch = new System.Diagnostics.Stopwatch();
+
+				stopwatch.Start();
+				System.Threading.Tasks.Parallel.For(0, deviceCount, deviceID =>
+				{
+					for(int i = 0; i < ITRATION; i++)
+					{
+						results[deviceID] += Length2(vecs[deviceID], N / deviceCount, deviceID);
+					}
+				});
+				stopwatch.Stop();
+
+				Console.WriteLine("1GPU: {0}", stopwatch.ElapsedMilliseconds);
+				Console.WriteLine("result = {0}", results.Sum());
+				Console.WriteLine("answer = {0}", VALUE * VALUE * ITRATION * N);
+
+				for(int deviceID = 0; deviceID < deviceCount; deviceID++)
+				{
+					Delete(vecs[deviceID], deviceID);
+				}
+			}
 
 			return Environment.ExitCode;
 		}
